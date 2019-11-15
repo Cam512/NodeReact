5520cdcdd5434fcc160c5d479e59e6c448a6e214
cambo512 <cameron.horejs@gmail.com>
2019-11-14 22:40:30 -0700

    Added auth to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,33 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return "Still deciding";
+
+      case false:
+        return "I am logged out";
+
+      default:
+        return "I am logged in";
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="left-brand-logo">Eboy</a>
-          <ul className="right">
-            <li>
-              <a>Login With Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
+function mapStateToProps({ auth }) {
+  return { auth };
+}
 
-export default Header;
+export default connect(mapStateToProps)(Header);
